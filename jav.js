5426f248008cc67b19aa82b36a9fdbbb85cbdc35
AdjarhoAndrew <105249390+AdjarhoAndrew@users.noreply.github.com>
2022-06-22 21:49:54 +0100

    Update jav.js

--- jav.js
+++ jav.js
@@ -1,1 +1,6 @@
 alert('NAME: ADJARHO ANDREW SLY, HEIGHT: 6.5FT, COUNTRY: NIGERIA');
+
+document.write('<h1>My Java Script Name Task');
+document.write('<h2>NAME: ADJARHO ANDREW SLY </h2>');
+document.write('<h2>HEIGHT: 6.5FT</h2>');
+document.write('<h2>COUNTRY: NIGERIA');
